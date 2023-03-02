309adf273f05223ee070a19573d57804502ab5c6
Hiram Amador <amadorhiramv@gmail.com>
2023-03-02 13:28:33 -0600

    Agregar Interfaz Principal

---
+++ SistemaGestiónPréstamos/VistaPrincipal.xaml.cs
@@ -0,0 +1,73 @@
+﻿using MySql.Data.MySqlClient;
+using MySql.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace SistemaGestiónPréstamos
+{
+    /// <summary>
+    /// Lógica de interacción para ViewPrincipal.xaml
+    /// </summary>
+    public partial class VistaPrincipal : Window
+    {
+        // MySqlConnection object to connect to the database
+        MySqlConnection conn = new MySqlConnection("server=localhost;user=root;password=Hiramsql2003;database=gestion_prestamos");
+
+        public VistaPrincipal()
+        {
+            InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg , int wParam, int lParam);
+
+        private void pnlBarraControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        // - Eventos de barra de control
+        private void pnlBarraControl_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void btnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void btnMax_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+            else this.WindowState = WindowState.Normal;     
+        }
+        private void btnMin_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+
+        }
+
+
+
+    }
+
+}
